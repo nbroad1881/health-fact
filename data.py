15d280c756fa7c68dbf89d40cf04af926954e03a
Nicholas Broad <nicholas@nmbroad.com>
2022-08-16 16:20:12 -0700

    simplify data processing
    move label2id to data config
    add get train/eval/test dataset functions

--- data.py
+++ data.py
@@ -1,62 +1,81 @@
+from typing import Dict, List, Bool
 from dataclasses import dataclass
 
-from datasets import load_dataset, DatasetDict
+import datasets
+from datasets import load_dataset
 from transformers import AutoTokenizer
 
 
 @dataclass
 class DataModule:
 
-    cfg: dict = None
+    cfg: Dict = None
 
     def __post_init__(self):
         if self.cfg is None:
             raise ValueError
 
+        self.tokenizer = AutoTokenizer.from_pretrained(
+            self.cfg.model.model_name_or_path,
+        )
+
+        self.label2id = self.cfg.data.label2id
+        self.id2label = {value: key for key, value in self.label2id.items()}
+
+    def prepare_dataset(self) -> None:
+        """
+        Load in dataset and tokenize.
+
+        If debugging, take small subset of the full dataset.
+        """
         self.raw_dataset = load_dataset("health_fact")
         self.raw_dataset = self.raw_dataset.filter(lambda x: x["label"] != -1)
 
-        self.tokenizer = AutoTokenizer.from_pretrained(
-            self.cfg["model_name_or_path"],
-            use_auth_token=True,
-        )
+        if self.cfg.data.n_rows > 0:
+            for split in self.raw_dataset:
+                self.raw_dataset[split] = self.raw_dataset[split].select(
+                    range(self.cfg.data.n_rows)
+                )
 
-    def prepare_dataset(self):
-
-        def tokenize(examples, indices):
-
-            tokenized =  self.tokenizer(
-                examples["claim"],
-                examples["main_text"],
-                padding=False,
-                truncation=self.cfg["truncation"],
-                stride=self.cfg["stride"],
-                max_length=self.cfg["max_seq_length"],
-                return_overflowing_tokens=self.cfg["stride"] is not None,
-            )
-
-            if self.cfg["stride"]:
-                labels = []
-                idxs = []
-
-                for o in tokenized["overflow_to_sample_mapping"]:
-                    idxs.append(indices[o])
-                    labels.append(examples["label"][o])
-
-                tokenized["idx"] = idxs
-                tokenized["labels"] = labels
-            else:
-                tokenized["labels"] = examples["label"]
-                
-
-            return tokenized
-        
-        
         cols = self.raw_dataset["train"].column_names
         self.tokenized_dataset = self.raw_dataset.map(
-            tokenize, 
-            batched=True, 
-            num_proc=self.cfg["num_proc"], 
-            remove_columns=cols, 
-            with_indices=True
+            self.tokenize,
+            batched=True,
+            num_proc=self.cfg.num_proc,
+            remove_columns=cols,
+            with_indices=True,
         )
+
+    def tokenize(self, examples: Dict[str, str], indices: List[str]):
+        """
+        Tokenize texts by putting claim text in front of main text.
+
+        If using a small model, can stride over the text.
+        """
+
+        tokenized = self.tokenizer(
+            examples["claim"],
+            examples["main_text"],
+            padding=False,
+            truncation=self.cfg.data.truncation,
+            max_length=self.cfg.data.max_seq_length,
+        )
+
+        tokenized["labels"] = examples["label"]
+
+        return tokenized
+
+    def get_train_dataset(self, tokenized: bool = True) -> datasets.Dataset:
+        if tokenized:
+            return self.tokenized_dataset["train"]
+        return self.raw_dataset["train"]
+
+    def get_eval_dataset(self, tokenized: bool = True) -> datasets.Dataset:
+        if tokenized:
+            return self.tokenized_dataset["validation"]
+        return self.raw_dataset["validation"]
+
+    def get_test_dataset(self, tokenized: bool = True) -> datasets.Dataset:
+        if tokenized:
+            return self.tokenized_dataset["test"]
+        return self.raw_dataset["test"]
