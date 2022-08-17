ab361fe1e2d4ff8474a1a6fc185a694fa70a69b7
Nicholas Broad <nicholas@nmbroad.com>
2022-08-16 22:52:33 -0700

    remove unnecessary config functions
    docstring for reinit functions
    link to wandbcallback

--- utils.py
+++ utils.py
@@ -36,69 +36,13 @@ def set_mlflow_env_vars(cfg):
     os.environ["MLFLOW_FLATTEN_PARAMS"] = cfg.mlflow.flatten_params
     os.environ["MLFLOW_RUN_ID"] = cfg.mlflow.run_id
 
-def fix_e(cfg):
-    """
-    When using "e notation" (1e-5) in a yaml file, it gets interpreted
-    as a string rather than a float. This function fixes that.
-    """
 
-    def fix(value):
-        pattern = r"\d+e\-\d+"
-        if re.search(pattern, value):
-            return eval(value)
-        return value
-
-
-    for k, v in cfg.items():
-        if isinstance(v, dict):
-            for kk, vv in v.items():
-                if isinstance(vv, str):
-                    cfg[k][kk] = fix(vv)
-        elif isinstance(v, str):
-            cfg[k] = fix(v)
-    
-    return cfg
-    
-def remove_defaults(cfg):
-    """
-    Since the yaml file indicates which arguments will
-    take default values, this function deletes those arguments
-    so that when TrainingArguments is called, all of the deleted
-    arguments will get default values.
-    """
-    to_remove = []
-    args = cfg["training_arguments"]
-    for key, value in args.items():
-        if value == "<default>":
-            to_remove.append(key)
-    
-    for key in to_remove:
-        del args[key]
-
-def get_configs(filepath):
-    """
-    Load config file.
-    Returns two dict objects.
-    The first has non-TrainingArgument arguments.
-    The second has TrainingArgument arguments.
-    """
-    with open(filepath) as fp:
-        cfg = yaml.safe_load(fp)
-
-    
-    remove_defaults(cfg)
-    cfg = fix_e(cfg)
-
-    # cfg["training_arguments"]["dataloader_num_workers"] = cfg["num_proc"]
-
-    training_args = cfg.pop("training_arguments")
-    return cfg, training_args
-
-
-class NewWandbCB(WandbCallback):
+class WandbCallbackV2(WandbCallback):
     """
     The current WandbCallback doesn't read some environment variables.
     This implementation fixes that.
+
+    https://github.com/huggingface/transformers/blob/d6eeb871706db0d64ab9ffd79f9545d95286b536/src/transformers/integrations.py#L566-L641
     """
     def __init__(self, run_config):
         super().__init__()
@@ -175,6 +119,12 @@ class NewWandbCB(WandbCallback):
 
 
 def reinit_model_weights(model, n_layers, config):
+    """
+    Revisting few-sample BERT tuning showed that re-initializing
+    the last few layers can be beneficial. This function helps do that.
+
+    https://github.com/asappresearch/revisit-bert-finetuning
+    """
 
     backbones = {
         "big_bird": "bert",
@@ -185,7 +135,6 @@ def reinit_model_weights(model, n_layers, config):
     if config.lstm:
         backbone_name = "transformer"
     else:
-
         backbone_name = backbones.get(config.model_type, config.model_type)
 
     backbone = getattr(model, backbone_name)
@@ -213,11 +162,17 @@ def reinit_model_weights(model, n_layers, config):
 
 
 def reinit_layers(layers, n_layers, std):
+    """
+    Helper function to reinitialize last n_layers
+    """
     for layer in layers[-n_layers:]:
         reinit_modules(layer.modules(), std)
 
 
 def reinit_modules(modules, std, reinit_embeddings=False):
+    """
+    Reinitializes every Linear, Embedding, and LayerNorm module provided.
+    """
     for module in modules:
         if isinstance(module, torch.nn.Linear):
             module.weight.data.normal_(mean=0.0, std=std)
