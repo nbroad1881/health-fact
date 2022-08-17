919681340c9c64cda64b1d9cfad0c8794d7f1e82
Nicholas Broad <nicholas@huggingface.co>
2022-08-17 03:25:31 +0000

    fix wandb tags, add mlflow env func

--- utils.py
+++ utils.py
@@ -15,13 +15,26 @@ def set_wandb_env_vars(cfg):
     Set environment variables from the config dict object.
     The environment variables can be picked up by wandb in Trainer.
     """
-    os.environ["WANDB_ENTITY"] = getattr(cfg, "entity", "")
-    os.environ["WANDB_PROJECT"] = getattr(cfg, "project", "")
-    os.environ["WANDB_RUN_GROUP"] = getattr(cfg, "group", "")
-    os.environ["WANDB_JOB_TYPE"] = getattr(cfg, "job_type", "")
-    os.environ["WANDB_NOTES"] = getattr(cfg, "notes", "")
-    os.environ["WANDB_TAGS"] = ",".join(getattr(cfg, "tags", ""))
 
+    os.environ["WANDB_ENTITY"] = getattr(cfg.wandb, "entity", "")
+    os.environ["WANDB_PROJECT"] = getattr(cfg.wandb, "project", "")
+    os.environ["WANDB_RUN_GROUP"] = getattr(cfg.wandb, "group", "")
+    os.environ["WANDB_JOB_TYPE"] = getattr(cfg.wandb, "job_type", "")
+    os.environ["WANDB_NOTES"] = getattr(cfg.wandb, "notes", "")
+    if cfg.wandb.tags:
+        os.environ["WANDB_TAGS"] = ",".join(cfg.wandb.tags)
+        
+def set_mlflow_env_vars(cfg):
+    """
+    Set environment variables from the config object.
+    The environment variables can be picked up by mlflow in Trainer.
+    """
+    
+    os.environ["HF_MLFLOW_LOG_ARTIFACTS"] = cfg.mlflow.log_artifact
+    os.environ["MLFLOW_NESTED_RUN"] = cfg.mlflow.nested_run
+    os.environ["MLFLOW_EXPERIMENT_NAME"] = cfg.mlflow.experiment_name
+    os.environ["MLFLOW_FLATTEN_PARAMS"] = cfg.mlflow.flatten_params
+    os.environ["MLFLOW_RUN_ID"] = cfg.mlflow.run_id
 
 def fix_e(cfg):
     """
@@ -136,7 +149,7 @@ class NewWandbCB(WandbCallback):
                     entity=os.getenv("WANDB_ENTITY", None),
                     id=os.getenv("WANDB_RUN_ID", None),
                     dir=os.getenv("WANDB_DIR", None),
-                    tags=tags if tags is None else tags.split(","),
+                    tags=tags.split(",") if tags else None,
                     job_type=os.getenv("WANDB_JOB_TYPE", None),
                     mode=os.getenv("WANDB_MODE", None),
                     anonymous=os.getenv("WANDB_ANONYMOUS", None),
